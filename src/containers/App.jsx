af823b1bdd91ef96b8421f07cf91a796aeaf0d2a
Lucía A. Sánchez H <sanchezhla@gmail.com>
2020-08-01 18:48:46 +0200

    error to paint component fixed and carousel done

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,12 +3,11 @@ import Header from '../components/Header';
 import Search from '../components/Search';
 import '../assets/styles/App.scss';
 
-const App = () => {
+const App = () => (
     <div className="App">
         <Header />
         <Search />
-
     </div>
-};
+);
 
 export default App;

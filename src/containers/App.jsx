a3fecee51d5713faa720f398191fae87ec3aed88
Lucía A. Sánchez H <sanchezhla@gmail.com>
2020-08-11 15:54:41 +0200

    Footer completion, Project customization for Bianca.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
-import '../assets/styles/App.scss';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss';
 
 const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+        <div className="App">
+            <Header />
+            <Search />
 
-        <Categories>
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-    </div>
+            <Categories>
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
 );
 
 export default App;
